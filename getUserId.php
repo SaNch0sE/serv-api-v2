06562084e2121cd312d34d821654fe447f60021f
sanch0se <alex.boiko.2001@gmail.com>
2020-02-26 20:20:51 +0200

    fix

--- getUserId.php
+++ getUserId.php
@@ -3,6 +3,8 @@
 	{
 		$users = json_decode(file_get_contents('users.json'), true);
 		foreach ($users as $key => $value) {
+			require_once "log.php";
+			mlog($value['user-key']);
 			if ($value['user-key'] === $_COOKIE['user-key']) {
 				return $value['id'];
 			}
